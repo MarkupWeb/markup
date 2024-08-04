a0fbdfd1d33f186b980e1a44e8859f30ef8ad5db
HassanGameill <hasanaboalion@gmail.com>
2024-08-04 14:41:02 +0300

    first commit

--- src/components/Partenrs/PrevArrow.tsx
+++ src/components/Partenrs/PrevArrow.tsx
@@ -4,8 +4,8 @@ import { HiChevronLeft } from "react-icons/hi2";
 
 function PrevArrow({onClick}: any) {
   return (
-    <div className="absolute right-0 -top-[80px] " onClick={onClick}>
-        <div className="bg-orange-500 h-[50px] w-[50px] rounded-full grid place-items-center cursor-pointer">
+    <div className="absolute z-10  top-[40%] " onClick={onClick}>
+        <div className="bg-[#ffffffc7] dark:bg-[#000000a0] dark:hover:bg-black shadow-2xl hover:bg-white h-[50px] w-[50px] rounded-full grid place-items-center cursor-pointer">
             <HiChevronLeft />
 
         </div>
