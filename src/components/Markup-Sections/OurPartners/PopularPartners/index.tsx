7ccfe59c85bba9faf2e7b1b69caf5b6d57d0055f
HassanGameill <hasanaboalion@gmail.com>
2024-08-19 13:43:23 +0300

    update

--- src/components/Markup-Sections/OurPartners/PopularPartners/index.tsx
+++ src/components/Markup-Sections/OurPartners/PopularPartners/index.tsx
@@ -6,7 +6,7 @@ const Popluar = () => {
   return (
     <div className='px-4s mx-auto max-w-2xl py-16 sm:px-6 lg:max-w-7xl'>
         <PopularPartners />
-        <PopularPartnerRTL />
+        
     </div>
   )
 }
