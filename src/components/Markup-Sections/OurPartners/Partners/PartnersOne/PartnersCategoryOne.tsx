b9db55e7422a861ad5e55d29d8fe7ea28039ea65
HassanGameill <hasanaboalion@gmail.com>
2024-09-11 19:58:03 +0300

    update content2

--- src/components/Markup-Sections/OurPartners/Partners/PartnersOne/PartnersCategoryOne.tsx
+++ src/components/Markup-Sections/OurPartners/Partners/PartnersOne/PartnersCategoryOne.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import SliderList from "@/components/Logic-List/MultipleSlider/SliderList";
 import PartnersDataOne from "./PartnersOneData";
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import CategoryCard from "../../../../Common/Categories/CategoryCard";
 import GridList from "@/components/Logic-List/GridList/GridList";
 import PartnerHeader from "@/components/Common/PartnerHeader/PartnerHeader";
@@ -12,23 +12,24 @@ function PartnersCategoryOne() {
 
   const t = useTranslations("PartnersOne");
   const PartnersContent = PartnersDataOne(t);
+  const local = useLocale();
+
 
   const records = PartnersContent;
 
   const renderCategories = (itemData) => (
     <div className="" key={itemData}>
-      <CategoryCard {...itemData}  />
+      <CategoryCard {...itemData} />
     </div>
   );
 
   return (
     <div className="container">
-      <PartnerHeader title="GREAT SUCCESS STORIES" />
+      <PartnerHeader title={local === "en" ? "Friends We Left Good Impact For Them" : "الأصدقاءالذي تركنا لهم تأثيرًا جيدًا"} />
 
-      <GridList
-        records={records}
-        renderItem={renderCategories}
-      />
+      <GridList records={records} renderItem={renderCategories} />
+
+      <GridList records={records} renderItem={renderCategories} />
     </div>
   );
 }
