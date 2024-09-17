75e0fcd26525a513ba18a69198cb7118061b1bb4
HassanGameill <hasanaboalion@gmail.com>
2024-09-17 15:37:29 +0300

    update6

--- src/components/Markup-Sections/Baner/Baner.tsx
+++ src/components/Markup-Sections/Baner/Baner.tsx
@@ -16,7 +16,7 @@ const Baner = () => {
         className=" flex items-center justify-center"
         target="_blank"
       >
-        <button className="relative inline-flex h-12 active:scale-95 transistion overflow-hidden rounded-lg p-[1px] focus:outline-none transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110  active:animate-bounce">
+        <button className="relative inline-flex h-12 active:scale-95 transistion overflow-hidden rounded-lg p-[1px] focus:outline-none transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110   active:animate-bounce shadow-2xl shadow-blue-800 dark:shadow-blue-500">
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#e7029a_0%,#f472b6_50%,#bd5fff_100%)] "></span>
           <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white hover:text-gray-300 backdrop-blur-3xl gap-2 undefined">
             Contact Us
