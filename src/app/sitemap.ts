c0b65fda1f8693bae5e77daaa4004656715ea393
HassanGameill <hasanaboalion@gmail.com>
2024-09-17 13:10:47 +0300

    uppdate SEO 3

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,39 +1,66 @@
-import type { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://markup.vip";
 
-    const baseUrl = "https://markup.vip";
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: `${baseUrl}/en`,
+          ar: `${baseUrl}/ar`,
+        },
+      },
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: `${baseUrl}/en/contact`,
+          ar: `${baseUrl}/ar/contact`,
+        },
+      },
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: `${baseUrl}/en/blog`,
+          ar: `${baseUrl}/ar/blog`,
+        },
+      },
+    },
 
-    return [
-        {
-            url: `${baseUrl}`,
-            lastModified: new Date(),
-            alternates: {
-                languages: {
-                    en: `${baseUrl}/en`,
-                    ar: `${baseUrl}/ar`,
-                },
-            },
+    {
+      url: `${baseUrl}/partners`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: `${baseUrl}/en/partners`,
+          ar: `${baseUrl}/ar/partners`,
         },
-        {
-            url: `${baseUrl}/contact`,
-            lastModified: new Date(),
-            alternates: {
-                languages: {
-                    en: `${baseUrl}/en/contact`,
-                    ar: `${baseUrl}/ar/contact`,
-                },
-            },
+      },
+    },
+
+    {
+      url: `${baseUrl}/questions`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: `${baseUrl}/en/questions`,
+          ar: `${baseUrl}/ar/questions`,
         },
-        {
-            url: `${baseUrl}/blog`,
-            lastModified: new Date(),
-            alternates: {
-                languages: {
-                    en: `${baseUrl}/en/blog`,
-                    ar: `${baseUrl}/ar/blog`,
-                },
-            },
-        },
-    ];
+      },
+    },
+
+
+
+
+
+
+  ];
 }
