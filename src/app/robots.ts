66c5e4dc6aefaa4fe64d74a82c9254eea91c5a53
HassanGameill <hasanaboalion@gmail.com>
2024-09-19 17:41:48 +0300

    update feature

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://markup.vip";
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://markup.vip" || "https://markupagency.net";
 
     return {
       rules: {
