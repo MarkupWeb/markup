a032b19f758acd56e85f1278abb7f8d7b0198229
HassanGameill <hasanaboalion@gmail.com>
2024-09-22 13:37:52 +0300

    update SEO

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ import createNextIntlPlugin from "next-intl/plugin"
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    siteUrl: "https://markup.vip",
+  generateRobotsTxt: true,
+  exclude: ["/sitemap.xml"], // <= exclude here
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      "https://markup.vip/sitemap.xml", // <==== Add here
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
