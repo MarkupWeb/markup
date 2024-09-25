5f2129ead91d41c9a286dd96bf1711eb0e0c0cb0
HassanGameill <hasanaboalion@gmail.com>
2024-09-25 19:56:35 +0300

    update data

--- src/components/Common/ScrollToTop/index.tsx
+++ src/components/Common/ScrollToTop/index.tsx
@@ -1,41 +1,32 @@
-"use client";
+"use client"
 import { useEffect, useState } from "react";
 
 export default function ScrollToTop() {
   const [isVisible, setIsVisible] = useState(false);
 
-  // Smooth scroll using requestAnimationFrame for better performance
+  // Custom smooth scroll to top with controllable speed
   const scrollToTop = () => {
-    const scrollStep = () => {
-      if (window.scrollY > 0) {
-        window.scrollBy(0, -window.scrollY / 10); // Adjust this factor to control speed
-        requestAnimationFrame(scrollStep);
+    const scrollStep = -window.scrollY / (1000 / 15); // 1000ms total scroll duration, adjust to control speed
+    const scrollInterval = setInterval(() => {
+      if (window.scrollY !== 0) {
+        window.scrollBy(0, scrollStep);
+      } else {
+        clearInterval(scrollInterval);
       }
-    };
-    requestAnimationFrame(scrollStep);
+    }, 15); // Adjust 15ms for slower/faster scroll
   };
 
   useEffect(() => {
-    // Debouncing scroll event for better performance
-    let timeoutId: NodeJS.Timeout;
-
     const toggleVisibility = () => {
-      if (timeoutId) {
-        clearTimeout(timeoutId);
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
       }
-      timeoutId = setTimeout(() => {
-        setIsVisible(window.pageYOffset > 300);
-      }, 100); // Adjust the debounce delay if needed
     };
 
     window.addEventListener("scroll", toggleVisibility);
-
-    return () => {
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      }
-      window.removeEventListener("scroll", toggleVisibility);
-    };
+    return () => window.removeEventListener("scroll", toggleVisibility);
   }, []);
 
   return (
@@ -43,10 +34,8 @@ export default function ScrollToTop() {
       {isVisible && (
         <div
           onClick={scrollToTop}
-          aria-label="Scroll to top"
-          tabIndex={0} // Make the button accessible via keyboard
-          role="button" // Define the role for accessibility
-          className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-md bg-blueMain dark:bg-orangeMain text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-lg focus:outline-none"
+          aria-label="scroll to top"
+          className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-md bg-blueMain dark:bg-orangeMain text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-lg"
         >
           <span className="mt-[6px] h-3 w-3 rotate-45 border-l border-t border-white"></span>
         </div>
